e281e688494dc4ea73ba07b86fe0bc786c9f1eb0
saba khubutia <bvb.xubutia@gmail.com>
2024-11-12 00:00:22 +0400

    finish js work

---
+++ main.js
@@ -0,0 +1,16 @@
+let userName = "saba khubutia";
+let dayofWeek = "Monday";
+let favoriteActivity = "play videogame";
+// console.log(userName, dayofWeek, favoriteActivity);
+console.log(
+  "Hi this is second text and my is",
+  userName,
+  "today is",
+  dayofWeek,
+  "a perfect day",
+  favoriteActivity
+);
+
+let textUser =
+  "Hi my name is 'saba khubutia' Today is 'dayofweek', a perfect day for 'play footbal'";
+console.log(textUser);
